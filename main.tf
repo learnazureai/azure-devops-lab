aa58ad7ec9fd41125af4723a1e38436f2ca9ac69
Ubuntu <azureuser@vm-devops-lab.icmej2pwwuaebob41n0bomxpnb.rx.internal.cloudapp.net>
2026-06-11 14:16:18 +0000

    Add HTTP NSG rule for nginx

--- main.tf
+++ main.tf
@@ -128,3 +128,18 @@ resource "azurerm_linux_virtual_machine" "main" {
     version   = "latest"
   }
 }
+
+resource "azurerm_network_security_rule" "http" {
+  name                       = "allow-http"
+  priority                   = 110
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
