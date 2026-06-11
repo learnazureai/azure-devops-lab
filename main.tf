4e58b46ef74f1e1469ab5e365df177856d294a75
Ubuntu <azureuser@vm-devops-lab.icmej2pwwuaebob41n0bomxpnb.rx.internal.cloudapp.net>
2026-06-11 10:15:20 +0000

    Add Linux VM deployment with Terraform

--- main.tf
+++ main.tf
@@ -74,3 +74,57 @@ resource "azurerm_subnet_network_security_group_association" "main" {
   subnet_id                 = azurerm_subnet.main.id
   network_security_group_id = azurerm_network_security_group.main.id
 }
+
+resource "azurerm_public_ip" "main" {
+  name                = "pip-devops-platform"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic-devops-platform"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "vm-devops-platform"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_D2s_v3"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8TbjXygJ+9P9AwJWHqh0KPjezoXP5FgxjBF90Zgcql azureuser@vm-devops-lab"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
